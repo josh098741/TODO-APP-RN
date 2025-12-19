48d0b82f30cd8fdf5e764c2eacfbdb1bbaa78075
josh098741 <morongemokaya@gmail.com>
2025-12-19 15:01:47 +0300

    Additional commits

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { Text, View,StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.content} >Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red"
+  },
+  content: {
+    fontSize: 20
+  },
+})
